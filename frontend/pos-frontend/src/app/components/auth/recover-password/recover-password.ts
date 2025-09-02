9001b7927abefd594ad43fafcf3f448bd00e515c
Cristian <crismust99@gmail.com>
2025-09-02 20:24:05 -0300

    create components, services and pages in frontend

---
+++ frontend/pos-frontend/src/app/components/auth/recover-password/recover-password.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recover-password',
+  imports: [],
+  templateUrl: './recover-password.html',
+  styleUrl: './recover-password.scss'
+})
+export class RecoverPassword {
+
+}
---
+++ frontend/pos-frontend/src/app/components/inventory/inventory-movements/inventory-movements.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-movements',
+  imports: [],
+  templateUrl: './inventory-movements.html',
+  styleUrl: './inventory-movements.scss'
+})
+export class InventoryMovements {
+
+}
